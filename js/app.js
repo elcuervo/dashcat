23ee7345193865cfec0283572be0760321e726ed
Bruno Aguirre <yo@brunoaguirre.com>
2012-12-06 17:44:33 -0200

    Proper scope for the token

--- js/app.js
+++ js/app.js
@@ -3,23 +3,35 @@ gui.Window.get().show();
 
 $(function() {
   var oauthToken = localStorage.getItem("oauth_token");
+
+  var getEvents = function(oauthToken) {
+    console.log(oauthToken);
+    $.ajax({
+      url: "https://api.github.com/users/elcuervo/received_events",
+      headers: {"Authorization": "token " + oauthToken},
+      success: function(data) {
+        var ul = $("#items");
+        $.each(data, function(i, item) {
+          var message = item.type + " from " + item.actor.login;
+
+          ul.append($("<li>").text(message));
+        })
+      }
+    });
+  }
+
   if(!oauthToken) {
     $.ajax({
       url: "https://api.github.com/authorizations",
       type: "POST",
       headers: {"Authorization": "Basic " + btoa("elcuervo:*********")},
-      data: JSON.stringify({ scopes: "notifications" }),
-      success: function(data) {
-        response = JSON.parse(data);
+      data: JSON.stringify({ scopes: "repo" }),
+      success: function(response) {
         localStorage.setItem("oauth_token", response.token)
+        getEvents(response.token);
       }
     });
+  } else {
+    getEvents(oauthToken);
   }
-  $.ajax({
-    url: "https://api.github.com/events",
-    headers: {"Authorization": "token " + oauthToken},
-    success: function(data) {
-      console.log(data);
-    }
-  });
 });
