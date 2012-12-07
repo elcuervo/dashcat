5e70a79c6cf494285d3b56e2bee2408f16555bcc
Bruno Aguirre <yo@brunoaguirre.com>
2012-12-07 12:52:40 -0200

    Add issue comments

--- js/app/main/collections/events.js
+++ js/app/main/collections/events.js
@@ -1,4 +1,5 @@
 var EventsCollection = Backbone.Collection.extend({
   model: Event,
-  url: "https://api.github.com/users/elcuervo/received_events"
+  //url: "https://api.github.com/users/elcuervo/received_events"
+  url: "https://api.github.com/events"
 });
--- js/app/main/views/event.js
+++ js/app/main/views/event.js
@@ -2,26 +2,22 @@ var EventView = Backbone.Marionette.ItemView.extend({
   tagName: "li",
 
   initialize: function() {
-    console.log(this.model.get("type"));
-    switch(this.model.get("type")) {
-      case "WatchEvent":
-        this.template = "#watch-event-template";
-        break;
+    var type = this.model.get("type");
+    var templateName = type.match(/[A-Z][a-z]+/g).join("-").toLowerCase();
 
-      case "PushEvent":
-        this.template = "#push-event-template";
-        break;
-
-      case "GistEvent":
-        this.template = "#gist-event-template";
-        break;
-    }
+    this.template = "#" + templateName + "-template";
+    if(!$(this.template).length) this.template = "#not-implemented-event-template";
   },
 
   templateHelpers: function() {
     var eventName = this.model.get("type").match(/[A-Z][a-z]+/g);
-    var eventClass = eventName.slice(0, -1).join().toLowerCase();
+    var eventClass = eventName.slice(0, -1).join("-").toLowerCase();
+    console.log(eventClass);
+
     var helpers = {
+      cleanUrl: function(url) {
+        return url.replace("api.", "\1")
+      },
       eventClass: eventClass
     }
 
@@ -34,6 +30,10 @@ var EventView = Backbone.Marionette.ItemView.extend({
 
 var Helpers = {
   PushEvent: {
+    sha: function(commit) {
+      return commit.sha.substr(0,7);
+    },
+
     branch: function() {
       var parts = this.payload.ref.split("/");
       return parts.pop();
