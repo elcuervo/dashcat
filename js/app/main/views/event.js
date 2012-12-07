3f909088ef70692c48d93f29b027bb946114aabc
Bruno Aguirre <yo@brunoaguirre.com>
2012-12-07 10:31:09 -0200

    Styles for different commits

--- js/app/main/views/event.js
+++ js/app/main/views/event.js
@@ -1,4 +1,28 @@
 var EventView = Backbone.Marionette.ItemView.extend({
   tagName: "li",
-  template: "#event-template"
+
+  initialize: function() {
+    switch(this.model.get("type")) {
+      case "WatchEvent":
+        this.template = "#watch-event-template";
+        break;
+
+      case "PushEvent":
+        this.template = "#push-event-template";
+        break;
+
+      case "GistEvent":
+        this.template = "#push-event-template";
+        break;
+    }
+  },
+
+  templateHelpers: function() {
+    var eventName = this.model.get("type").match(/[A-Z][a-z]+/g);
+    var eventClass = eventName.slice(0, -1).join().toLowerCase();
+
+    return {
+      eventClass: eventClass
+    }
+  }
 });
