da66a7110b2b46bd706a7602cca8cdcdcc9c2237
Bruno Aguirre <yo@brunoaguirre.com>
2012-12-14 12:25:40 -0200

    Colorful Diff and merge icons

--- html/js/app/main/views/pull_request.js
+++ html/js/app/main/views/pull_request.js
@@ -3,9 +3,24 @@ var PullRequestView = BaseItemView.extend({
 
   template: "#pull-request-template",
 
+  events: {
+    "click .diff": "loadDiff"
+  },
+
   helpers: function() {
     return {
       mergeState: "state-" + this.model.get("pull_info").mergeable_state
     }
+  },
+
+  loadDiff: function() {
+    var diffUrl = this.model.get("pull_request").diff_url;
+    var test = document.createElement("script");
+
+    test.type = "text/diff";
+    test.src = diffUrl;
+    test.id = "bla";
+    $(document).append(test);
+    console.log(test);
   }
 });
